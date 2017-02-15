e48686e6d9828ec9f5961059c27d4cce15bed0fc
wowua <wowua@wowuas-MacBook-Pro.local>
2017-02-15 15:00:49 +0800

    l, c prefixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-const fs = require('fs');
+const gulp = require('gulp')
+	, pug = require('gulp-pug')
+	, fs = require('fs')
+	, browserSync = require('browser-sync').create()
+	, reload = browserSync.reload
+	, plumber = require('gulp-plumber');
+
+
+// server
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./dist"
+		}
+	});
+});
 
 gulp.task('pug', () => {
 	// let locals = require('./content.json');
 
 	gulp.src('src/pages/**/*.pug')
+		.pipe(plumber())
 		.pipe(pug({
-			locals : JSON.parse(fs.readFileSync('./content.json', 'utf8')),
+			locals : {
+				one : JSON.parse(fs.readFileSync('./content.json', 'utf8')),
+				two : JSON.parse(fs.readFileSync('./content2.json', 'utf8'))
+			},
 			pretty: true,
 		}))
 		.pipe(gulp.dest('dist'))
+		.pipe(reload({stream : true}));
 });
 
 gulp.task('watch', () => {
 	gulp.watch('src/**/*.pug', ['pug'])
 });
 
-gulp.task('default', ['pug', 'watch']);
+gulp.task('default', ['server', 'pug', 'watch']);
